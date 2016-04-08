d171d61af3453047f865b2b61d22af969bd0fe43
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-08 17:30:54 +0200

    Add a denoising script (FFT).

---
+++ denoising_with_fft.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Jérémie DECOCK (http://www.jdhp.org)
+
+# This script is provided under the terms and conditions of the MIT license:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Denoise FITS images with Discrete Fourier Transform.
+
+This script originally came from
+https://github.com/jeremiedecock/snippets/blob/master/python/numpy/fft_transform/fft2.py.
+
+Example usages:
+  ./denoising_with_fft.py -h
+  ./denoising_with_fft.py -t 0.0001 -s ./test.jpeg
+  ./denoising_with_fft.py -t 0.001 ./test.jpeg
+  ipython3 -- ./denoising_with_fft.py -t 0.0001 -s ./test.jpeg
+
+This snippet requires Numpy, Scipy, Matplotlib and PIL/Pillow Python libraries.
+
+Additional documentation:
+- Numpy implementation: http://docs.scipy.org/doc/numpy/reference/routines.fft.html
+- Scipy implementation: http://docs.scipy.org/doc/scipy/reference/fftpack.html
+"""
+
+import argparse
+import os
+import numpy as np
+
+import utils
+
+def main():
+
+    # PARSE OPTIONS ###########################################################
+
+    parser = argparse.ArgumentParser(description="Denoise FITS images with DFT.")
+
+    parser.add_argument("--shift", "-s", action="store_true", default=False,
+                        help="Shift the zero to the center")
+    parser.add_argument("--threshold", "-t", type=float, default=0, metavar="FLOAT", 
+                        help="The threshold value (between 0 and 1)")
+    parser.add_argument("fileargs", nargs=1, metavar="FILE",
+                        help="The file image to process (FITS or PNG)")
+    args = parser.parse_args()
+
+    shift = args.shift
+    threshold = args.threshold
+    input_file_path = args.fileargs[0]
+
+    base_file_path = os.path.splitext(input_file_path)[0]
+
+    # READ THE INPUT FILE ######################################################
+
+    input_img = utils.get_image_array_from_file(input_file_path)
+
+    if input_img.ndim != 2:
+        raise Exception("Unexpected error: the input FITS file should contain a 2D array.")
+
+
+    # FOURIER TRANSFORM WITH NUMPY ############################################
+
+    # Do the fourier transform #############
+
+    transformed_img = np.fft.fft2(input_img)
+
+    if shift:
+        transformed_img = np.fft.fftshift(transformed_img)
+
+    #utils.plot_image(np.log10(abs(transformed_img)),
+    #                 title="Fourier coefficients before filtering")
+    #utils.save_image(np.log10(abs(transformed_img)),
+    #                 "{}_dft_fourier_coefficients_before_filtering.pdf".format(base_file_path),
+    #                 title="Fourier coefficients before filtering")
+
+
+    # Compute the standard deviation of the plane ###########
+
+    # TODO
+#    img_sigma = np.std(transformed_img)
+    max_value = np.max(abs(transformed_img))
+
+    # Apply a threshold on the transformed image ############
+
+    # TODO
+#    img_mask = abs(transformed_img) > (img_sigma * 3.)  
+    img_mask =  abs(transformed_img) > (max_value * threshold)
+    filtered_transformed_img = transformed_img * img_mask
+
+    utils.plot_image(np.log10(abs(filtered_transformed_img)),
+                     title="Fourier coefficients after filtering")
+    #utils.save_image(np.log10(abs(filtered_transformed_img)),
+    #                 "{}_dft_fourier_coefficients_after_filtering.pdf".format(base_file_path),
+    #                 title="Fourier coefficients after filtering")
+
+
+    # Do the reverse transform #############
+
+    if shift:
+        filtered_transformed_img = np.fft.ifftshift(filtered_transformed_img)
+
+    filtered_img = np.fft.ifft2(filtered_transformed_img)
+
+    utils.plot_image(abs(filtered_img),
+                     title="Denoised image")
+    #utils.save_image(abs(filtered_img),
+    #                 "{}_dft_denoised.pdf".format(base_file_path),
+    #                 title="Denoised image")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ utils.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Jérémie DECOCK (http://www.jdhp.org)
+
+# This script is provided under the terms and conditions of the MIT license:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from astropy.io import fits
+
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+import PIL.Image as pil_img     # PIL.Image is a module not a class...
+
+def get_image_array_from_file(file_path):
+    """
+    Return the image array contained in the first HDU of the given FITS file.
+    """
+    
+    file_extension = os.path.splitext(file_path)[1].lower()
+
+    if file_extension == ".fits":
+
+        hdu_list = fits.open(file_path)   # open the FITS file
+
+        if len(hdu_list) != 1:
+            raise Exception("The FITS file should contain only one HDU.")
+
+        image_array = hdu_list[0].data    # "hdu.data" is a Numpy Array
+
+        hdu_list.close()
+
+    elif file_extension == ".png":
+
+        # Open the image and convert it to grayscale
+        image_array = np.array(pil_img.open(file_path).convert('L'))
+
+    else:
+
+        raise Exception("Unrecognized input file format.")
+
+    return image_array
+
+
+# MATPLOTLIB ##################################################################
+
+def save_image(img, output_file_path, title=""):
+    """
+    img should be a 2D numpy array.
+    """
+    fig = plt.figure(figsize=(8.0, 8.0))
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+    ax.imshow(img, interpolation='nearest', cmap=cm.gray)
+    plt.savefig(output_file_path)
+    plt.close('all')
+
+
+def plot_image(img, title=""):
+    """
+    img should be a 2D numpy array.
+    """
+    fig = plt.figure(figsize=(8.0, 8.0))
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+    ax.imshow(img, interpolation='nearest', cmap=cm.gray)
+    plt.show()
+
