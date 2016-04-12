4c430dd0befe8297af9ccaddafa13c42e56d1f5f
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-12 09:33:08 +0200

    Fix a small bug.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+export PYTHONPATH=.:$PYTHONPATH
 INFILE=data/CT062.fits
 
-./datapipe/denoising/tailcut -T 0.75 -t 0.5 "${INFILE}"
+./datapipe/denoising/tailcut.py -T 0.75 -t 0.5 "${INFILE}"
 ./datapipe/denoising/fft.py -s -t 0.02 "${INFILE}"
 ./datapipe/denoising/wavelets_mrtransform.py "${INFILE}"
