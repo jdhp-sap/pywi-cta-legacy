0047306164d77664c0bc88a988b423842f26c8ce
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-13 10:38:19 +0200

    Misc updates.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 export PYTHONPATH=.:$PYTHONPATH
-INFILE=data/CT062.fits
+
+INFILE=$1
+#INFILE=data/CT062.fits
 
 ./datapipe/denoising/tailcut.py -T 0.75 -t 0.5 "${INFILE}"
 ./datapipe/denoising/fft.py -s -t 0.02 "${INFILE}"
