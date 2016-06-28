14ae3ee574f53a1d56c1829a6d9efa0b599d4223
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-06-28 19:47:12 +0200

    Add a run script.

---
+++ xp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export PYTHONPATH=.:$PYTHONPATH
+
+./datapipe/denoising/fft.py -b1 -s -t0.004 ~/data/sim_013/*.fits
+./datapipe/denoising/tailcut.py -b1 -T0.75 -t0.5 ~/data/sim_013/*.fits
+./datapipe/denoising/wavelets_mrtransform.py -b1 -n5 ~/data/sim_013/*.fits
