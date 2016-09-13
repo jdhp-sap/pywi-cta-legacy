21775e8a36aa601b5d6ddc8f215020db251ea6bf
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-09-13 17:31:25 +0200

    Misc updates.

--- xp.sh
+++ xp.sh
@@ -5,15 +5,19 @@ export PYTHONPATH=.:$PYTHONPATH
 #./datapipe/denoising/fft.py -b1 -s -t0.004           $(find ~/data/astri_mini_array/fits/proton -type f -name "*.fits" | head -n 2203)
 #./datapipe/denoising/tailcut.py -b1 -T0.75 -t0.5     $(find ~/data/astri_mini_array/fits/proton -type f -name "*.fits" | head -n 2203)
 #./datapipe/denoising/wavelets_mrtransform.py -b1 -n5 $(find ~/data/astri_mini_array/fits/proton -type f -name "*.fits" | head -n 2203)
+#rm *.fits
 
 #./datapipe/denoising/fft.py -b2 -s -t0.004           $(find ~/data/astri_mini_array/fits/proton -type f -name "*.fits" | head -n 2203)
 #./datapipe/denoising/tailcut.py -b2 -T0.75 -t0.5     $(find ~/data/astri_mini_array/fits/proton -type f -name "*.fits" | head -n 2203)
 #./datapipe/denoising/wavelets_mrtransform.py -b2 -n5 $(find ~/data/astri_mini_array/fits/proton -type f -name "*.fits" | head -n 2203)
+#rm *.fits
 
 ./datapipe/denoising/fft.py -b1 -s -t0.004           $(find ~/data/astri_mini_array/fits/gamma -type f -name "*.fits" | head -n 2203)
 ./datapipe/denoising/tailcut.py -b1 -T0.75 -t0.5     $(find ~/data/astri_mini_array/fits/gamma -type f -name "*.fits" | head -n 2203)
 ./datapipe/denoising/wavelets_mrtransform.py -b1 -n5 $(find ~/data/astri_mini_array/fits/gamma -type f -name "*.fits" | head -n 2203)
+rm *.fits
 
 #./datapipe/denoising/fft.py -b2 -s -t0.004           $(find ~/data/astri_mini_array/fits/gamma -type f -name "*.fits" | head -n 2203)
 #./datapipe/denoising/tailcut.py -b2 -T0.75 -t0.5     $(find ~/data/astri_mini_array/fits/gamma -type f -name "*.fits" | head -n 2203)
 #./datapipe/denoising/wavelets_mrtransform.py -b2 -n5 $(find ~/data/astri_mini_array/fits/gamma -type f -name "*.fits" | head -n 2203)
+#rm *.fits
