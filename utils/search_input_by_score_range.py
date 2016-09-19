227d2d812b1842470f5f26c6e5c9aca54c860f59
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-09-19 19:21:14 +0200

    Add a tool script.

---
+++ utils/search_input_by_score_range.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Search inputs by score range (in score JSON files).
+"""
+
+import argparse
+import json
+import sys
+
+import numpy as np
+
+
+def fetch_score(json_file_path):
+    with open(json_file_path, "r") as fd:
+        score_dict = json.load(fd)
+    return score_dict
+
+
+if __name__ == '__main__':
+
+    # PARSE OPTIONS ###########################################################
+
+    parser = argparse.ArgumentParser(description="Make statistics on score files (JSON files).")
+
+    parser.add_argument("--min", "-m", type=float, default=None, metavar="FLOAT", 
+                        help="The lower bound of the selected range")
+
+    parser.add_argument("--max", "-M", type=float, default=None, metavar="FLOAT", 
+                        help="The upper bound of the selected range")
+
+    parser.add_argument("--index", "-i", type=int, default=0, metavar="INT", 
+                        help="The index of the score to plot in case of multivalued scores")
+
+    parser.add_argument("fileargs", nargs=1, metavar="FILE",
+                        help="The JSON file to process")
+
+    args = parser.parse_args()
+
+    min_score = args.min
+    max_score = args.max
+    score_index = args.index
+    json_file_path = args.fileargs[0]
+
+    # FETCH SCORE #############################################################
+
+    score_dict = fetch_score(json_file_path)
+
+    input_file_path_list = score_dict["input_file_path_list"]
+    score_list = [float(score[score_index]) for score in score_dict["score_list"]]
+
+    data_list = zip(input_file_path_list, score_list)
+
+    # SEARCH INPUTS BY SCORE RANGE ############################################
+
+    if min_score is None:
+        min_score = min(score_list)
+
+    if max_score is None:
+        max_score = max(score_list)
+
+    filtered_data_list = [item for item in data_list if ((item[1] >= min_score) and (item[1] <= max_score))]
+
+    filtered_data_list = sorted(filtered_data_list, key=lambda item: item[1])
+
+    print("Min:", min_score, file=sys.stderr)
+    print("Max:", max_score, file=sys.stderr)
+    print(len(filtered_data_list), "inputs", file=sys.stderr)
+
+    for file_path, score in filtered_data_list:
+        #print(file_path)
+        print(file_path, score)
+
