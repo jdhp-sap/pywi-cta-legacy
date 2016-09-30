4f427bedcc72b869567cdaa0e3997bb26997bc33
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-09-30 13:24:18 +0200

    Add options to print the N best or the N worst images.

--- utils/search_input_by_score_range.py
+++ utils/search_input_by_score_range.py
@@ -24,13 +24,19 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Make statistics on score files (JSON files).")
 
-    parser.add_argument("--min", "-m", type=float, default=None, metavar="FLOAT", 
+    parser.add_argument("--best", "-b", type=int, default=None, metavar="INT",
+                        help="Select the N best images")
+
+    parser.add_argument("--worst", "-w", type=int, default=None, metavar="INT",
+                        help="Select the N worst images")
+
+    parser.add_argument("--min", "-m", type=float, default=None, metavar="FLOAT",
                         help="The lower bound of the selected range")
 
-    parser.add_argument("--max", "-M", type=float, default=None, metavar="FLOAT", 
+    parser.add_argument("--max", "-M", type=float, default=None, metavar="FLOAT",
                         help="The upper bound of the selected range")
 
-    parser.add_argument("--index", "-i", type=int, default=0, metavar="INT", 
+    parser.add_argument("--index", "-i", type=int, default=0, metavar="INT",
                         help="The index of the score to plot in case of multivalued scores")
 
     parser.add_argument("fileargs", nargs=1, metavar="FILE",
@@ -38,6 +44,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    num_best = args.best
+    num_worst = args.worst
     min_score = args.min
     max_score = args.max
     score_index = args.index
@@ -60,9 +68,18 @@ if __name__ == '__main__':
     if max_score is None:
         max_score = max(score_list)
 
+
     filtered_data_list = [item for item in data_list if ((item[1] >= min_score) and (item[1] <= max_score))]
 
     filtered_data_list = sorted(filtered_data_list, key=lambda item: item[1])
+
+
+    if num_best is not None:
+        filtered_data_list = filtered_data_list[:num_best]
+
+    if num_worst is not None:
+        filtered_data_list = filtered_data_list[-num_worst:]
+
 
     print("Min:", min_score, file=sys.stderr)
     print("Max:", max_score, file=sys.stderr)
