e633d9448227062326fc1784af4a848d530287bd
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-10-07 16:30:45 +0200

    Add params for wavelets_mrfilter.

--- datapipe/denoising/wavelets_mrfilter.py
+++ datapipe/denoising/wavelets_mrfilter.py
@@ -71,7 +71,14 @@ class WaveletTransform(AbstractCleaningAlgorithm):
         super(WaveletTransform, self).__init__()
         self.label = "WT (mr_filter)"  # Name to show in plots
 
-    def clean_image(self, input_img, number_of_scales=4):
+    def clean_image(self,
+                    input_img,
+                    number_of_scales=4,
+                    suppress_last_scale=True,
+                    suppress_isolated_pixels=True,
+                    coef_detection_method=1,
+                    k_sigma_noise_threshold=3,
+                    noise_model=3):
         """
         Do the wavelet transform.
 
@@ -113,8 +120,20 @@ class WaveletTransform(AbstractCleaningAlgorithm):
             # EXECUTE MR_FILTER ####################################
 
             # TODO: improve the following lines
+            cmd = 'mr_filter'
+            cmd += ' -n{}'.format(number_of_scales)
+            cmd += ' -K' if suppress_last_scale else ''
+            cmd += ' -k' if suppress_isolated_pixels else ''
+            cmd += ' -C{}'.format(coef_detection_method)
+            cmd += ' -s{}'.format(k_sigma_noise_threshold)
+            cmd += ' -m{}'.format(noise_model)
+            cmd += ' "{}" {}'.format(input_file_path, mr_output_file_path)
+
+            #cmd = 'mr_filter -K -k -C1 -s3 -m3 -n{} "{}" {}'.format(number_of_scales, input_file_path, mr_output_file_path)
             #cmd = 'mr_filter -K -k -C1 -s3 -m2 -p -P -n{} "{}" {}'.format(number_of_scales, input_file_path, mr_output_file_path)
-            cmd = 'mr_filter -K -k -C1 -s3 -m3 -n{} "{}" {}'.format(number_of_scales, input_file_path, mr_output_file_path)
+
+            if self.verbose:
+                print(cmd)
 
             try:
                 os.system(cmd)
@@ -145,7 +164,38 @@ def main():
     parser = argparse.ArgumentParser(description="Denoise FITS images with Wavelet Transform.")
 
     parser.add_argument("--number_of_scales", "-n", type=int, default=4, metavar="INTEGER",
-                        help="number of scales used in the multiresolution transform (default: 4)")
+                        help="Number of scales used in the multiresolution transform")
+
+    parser.add_argument("--suppress-last-scale", "-K", action="store_true",
+                        help="Suppress the last scale (to have background pixels = 0)")
+
+    parser.add_argument("--suppress-isolated-pixels", "-k", action="store_true",
+                        help="Suppress isolated pixels in the support")
+
+    parser.add_argument("--coef-detection-method", "-C", type=int, default=1, metavar="INTEGER",
+                        help="""Coef_Detection_Method:
+                            1: K-SigmaNoise Threshold
+                            2: False Discovery Rate (FDR) Theshold
+                            3: Universal Threshold
+                            4: SURE Threshold
+                            5: Multiscale SURE Threshold
+                            default is K-SigmaNoise Threshold""")
+
+    parser.add_argument("--k-sigma-noise-threshold", "-s", type=float, default=3, metavar="FLOAT",
+                        help="Thresholding at nsigma * SigmaNoise")
+
+    parser.add_argument("--noise-model", "-m", type=int, default=3, metavar="INTEGER",
+                        help="""Noise model:
+                            1: Gaussian noise
+                            2: Poisson noise
+                            3: Poisson noise + Gaussian noise
+                            4: Multiplicative noise
+                            5: Non-stationary additive noise
+                            6: Non-stationary multiplicative noise
+                            7: Undefined stationary noise
+                            8: Undefined noise
+                            9: Stationary correlated noise
+                            10: Poisson noise with few events""")
 
     # COMMON OPTIONS
 
@@ -171,6 +221,12 @@ def main():
     args = parser.parse_args()
 
     number_of_scales = args.number_of_scales
+    suppress_last_scale = args.suppress_last_scale
+    suppress_isolated_pixels = args.suppress_isolated_pixels
+    coef_detection_method = args.coef_detection_method
+    k_sigma_noise_threshold = args.k_sigma_noise_threshold
+    noise_model = args.noise_model
+
     benchmark_method = args.benchmark
     plot = args.plot
     saveplot = args.saveplot
@@ -182,7 +238,14 @@ def main():
     else:
         output_file_path = args.output
 
-    cleaning_function_params = {"number_of_scales": number_of_scales}
+    cleaning_function_params = {
+                "number_of_scales": number_of_scales,
+                "suppress_last_scale": suppress_last_scale,
+                "suppress_isolated_pixels": suppress_isolated_pixels,
+                "coef_detection_method": coef_detection_method,
+                "k_sigma_noise_threshold": k_sigma_noise_threshold,
+                "noise_model": noise_model,
+            }
 
     cleaning_algorithm = WaveletTransform()
     cleaning_algorithm.run(cleaning_function_params,
