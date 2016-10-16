5794e06d0b378e3c88ee28ffab0f208030a2f26f
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-10-16 16:57:10 +0200

    Misc upates.

--- make_parallel_instances.sh
+++ make_parallel_instances.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for INST_NUM in $(seq 16)
+for INST_NUM in $(seq 24)
 do
     echo ${INST_NUM}
     git clone https://github.com/jdhp-sap/sap-cta-data-pipeline.git ../sap-cta-data-pipeline_${INST_NUM}
