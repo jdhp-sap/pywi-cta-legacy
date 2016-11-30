df6fd9eb0dae46bd425548fe85ff110cda7c3c99
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-11-30 19:47:20 +0100

    Update xp.sh.

--- xp.sh
+++ xp.sh
@@ -6,14 +6,15 @@ source activate cta
 NUM_IMG=1000
 
 TAILCUT_PARAMS="-T10 -t5"                    # REFERENCE
-WT_MR_FILTER_PARAMS="-n4 -K -k -C1 -s3 -m3"  # REFERENCE
+WT_MR_FILTER_PARAMS="-n4 -K -k -C1 -s3 -m3"  # 2016-10-19 REFERENCE
+#WT_MR_FILTER_PARAMS="-n4 -K -k -C2 -m3"
 
 # PROTONS #########
 
 #echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b mpdspd ${TAILCUT_PARAMS}       -o score_proton_mpdspd_tailcut_jd.json         $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
-#echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut.py           -b mpdspd ${TAILCUT_PARAMS}       -o score_proton_mpdspd_tailcut.json            $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
-#echo "* PROTON WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b mpdspd ${WT_MR_FILTER_PARAMS}  -o score_proton_mpdspd_wavelets_mrfilter.json  $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
-#for FILE in .tmp*.fits ; do rm $FILE ; done
+echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut.py           -b mpdspd ${TAILCUT_PARAMS}       -o score_proton_mpdspd_tailcut.json            $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
+echo "* PROTON WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b mpdspd ${WT_MR_FILTER_PARAMS}  -o score_proton_mpdspd_wavelets_mrfilter.json  $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
+for FILE in .tmp*.fits ; do rm $FILE ; done
 
 # GAMMAS ##########
 
