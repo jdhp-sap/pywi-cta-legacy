b04f2b743bad310d56f8a0f77c53e04380cfe6c1
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-12-01 16:18:14 +0100

    Misc updates.

--- xp_plot.sh
+++ xp_plot.sh
@@ -5,8 +5,8 @@ export PYTHONPATH=.:$PYTHONPATH
 # PLOT SCORES #####
 
 echo "" # & echo "* SCORE PROTON MPD BOXPLOT ***********" & ./utils/plot_score_boxplot.py   -q                   --title "Score MPD (protons)"        -o score_proton_mpd_boxplot.pdf                       score_proton_mpd_*.json
-echo ""   & echo "* SCORE PROTON MPDSPD I0 BOXPLOT *****" & ./utils/plot_score_boxplot.py   -q               -i0 --title "$\mathcal{E}_1$ (protons)"  -o score_proton_mpdspd_i0_boxplot.pdf                 score_proton_mpdspd_*.json
-echo ""   & echo "* SCORE PROTON MPDSPD I1 BOXPLOT *****" & ./utils/plot_score_boxplot.py   -q               -i1 --title "$\mathcal{E}_2$ (protons)"  -o score_proton_mpdspd_i1_boxplot.pdf                 score_proton_mpdspd_*.json
+echo "" # & echo "* SCORE PROTON MPDSPD I0 BOXPLOT *****" & ./utils/plot_score_boxplot.py   -q               -i0 --title "$\mathcal{E}_1$ (protons)"  -o score_proton_mpdspd_i0_boxplot.pdf                 score_proton_mpdspd_*.json
+echo "" # & echo "* SCORE PROTON MPDSPD I1 BOXPLOT *****" & ./utils/plot_score_boxplot.py   -q               -i1 --title "$\mathcal{E}_2$ (protons)"  -o score_proton_mpdspd_i1_boxplot.pdf                 score_proton_mpdspd_*.json
 echo "" # & echo "* SCORE PROTON SSPD BOXPLOT **********" & ./utils/plot_score_boxplot.py   -q                   --title "Score SSPD (protons)"       -o score_proton_sspd_boxplot.pdf                      score_proton_sspd_*.json
 
 echo "" # & echo "* SCORE PROTON MPD HISTOGRAM *********" & ./utils/plot_score_histogram.py -q --logx --logy     --title "Score MPD (protons)"        -o score_proton_mpd_histogram.pdf                     score_proton_mpd_*.json
@@ -17,8 +17,8 @@ echo ""   & echo "* SCORE PROTON SSPD HISTOGRAM ********" & ./utils/plot_score_h
 echo ""   & echo "* SCORE PROTON SSPD HISTOGRAM ********" & ./utils/plot_score_histogram.py -q        --logy     --title "WT"                         -o score_proton_sspd_histogram_wavelets_mrfilter.pdf  score_proton_sspd_wavelets_mrfilter.json
 
 echo "" # & echo "* SCORE GAMMA MPD BOXPLOT ************" & ./utils/plot_score_boxplot.py   -q                   --title "Score MPD ($\gamma$)"       -o score_gamma_mpd_boxplot.pdf                        score_gamma_mpd_*.json
-echo ""   & echo "* SCORE GAMMA MPDSPD I0 BOXPLOT ******" & ./utils/plot_score_boxplot.py   -q               -i0 --title "$\mathcal{E}_1$ ($\gamma$)" -o score_gamma_mpdspd_i0_boxplot.pdf                  score_gamma_mpdspd_*.json
-echo ""   & echo "* SCORE GAMMA MPDSPD I1 BOXPLOT ******" & ./utils/plot_score_boxplot.py   -q               -i1 --title "$\mathcal{E}_2$ ($\gamma$)" -o score_gamma_mpdspd_i1_boxplot.pdf                  score_gamma_mpdspd_*.json
+echo "" # & echo "* SCORE GAMMA MPDSPD I0 BOXPLOT ******" & ./utils/plot_score_boxplot.py   -q               -i0 --title "$\mathcal{E}_1$ ($\gamma$)" -o score_gamma_mpdspd_i0_boxplot.pdf                  score_gamma_mpdspd_*.json
+echo "" # & echo "* SCORE GAMMA MPDSPD I1 BOXPLOT ******" & ./utils/plot_score_boxplot.py   -q               -i1 --title "$\mathcal{E}_2$ ($\gamma$)" -o score_gamma_mpdspd_i1_boxplot.pdf                  score_gamma_mpdspd_*.json
 echo "" # & echo "* SCORE GAMMA SSPD BOXPLOT ***********" & ./utils/plot_score_boxplot.py   -q                   --title "Score SSPD ($\gamma$)"      -o score_gamma_sspd_boxplot.pdf                       score_gamma_sspd_*.json
 
 echo "" # & echo "* SCORE GAMMA MPD HISTOGRAM **********" & ./utils/plot_score_histogram.py -q --logx --logy     --title "Score MPD ($\gamma$)"       -o score_gamma_mpd_histogram.pdf                      score_gamma_mpd_*.json
