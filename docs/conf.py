08af3065f1cf257a1d889b45ff412e742be4508c
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-12-02 14:03:49 +0100

    Use Napoleon and RTD extensions and themes (that highly improve the sphinx docs).

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -132,7 +133,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 # html_theme = 'default'
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
