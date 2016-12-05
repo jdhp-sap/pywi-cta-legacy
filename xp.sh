ac7a4a26719d69838dd24137db2e6f3925bafa59
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-12-05 11:25:54 +0100

    Fix a mistake.

--- xp.sh
+++ xp.sh
@@ -12,21 +12,21 @@ WT_MR_FILTER_PARAMS="-n4 -K -k -C1 -s3 -m3"  # 2016-10-19 REFERENCE
 
 # PROTONS #########
 
-#echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b all ${TAILCUT_PARAMS}       -o score_proton_mpdspd_tailcut_jd.json         $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
-echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut.py           -b all ${TAILCUT_PARAMS}       -o score_proton_mpdspd_tailcut.json            $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
-echo "* PROTON WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b all ${WT_MR_FILTER_PARAMS}  -o score_proton_mpdspd_wavelets_mrfilter.json  $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
+#echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b all ${TAILCUT_PARAMS}       -o score_proton_all_tailcut_jd.json         $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
+echo "* PROTON TAILCUT"  & ./datapipe/denoising/tailcut.py           -b all ${TAILCUT_PARAMS}       -o score_proton_all_tailcut.json            $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
+echo "* PROTON WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b all ${WT_MR_FILTER_PARAMS}  -o score_proton_all_wavelets_mrfilter.json  $(find ~/astri_data/fits/proton -type f -name "*.fits" | head -n ${NUM_IMG})
 for FILE in .tmp*.fits ; do rm $FILE ; done
 
 # GAMMAS ##########
 
-#echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b all ${TAILCUT_PARAMS}       -o score_gamma_mpdspd_tailcut_jd.json          $(find ~/astri_data/fits/gamma -type f -name "*.fits" | head -n ${NUM_IMG})
-echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut.py           -b all ${TAILCUT_PARAMS}       -o score_gamma_mpdspd_tailcut.json             $(find ~/astri_data/fits/gamma -type f -name "*.fits" | head -n ${NUM_IMG})
-echo "* GAMMA WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b all ${WT_MR_FILTER_PARAMS}  -o score_gamma_mpdspd_wavelets_mrfilter.json   $(find ~/astri_data/fits/gamma -type f -name "*.fits" | head -n ${NUM_IMG})
+#echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b all ${TAILCUT_PARAMS}       -o score_gamma_all_tailcut_jd.json          $(find ~/astri_data/fits/gamma -type f -name "*.fits" | head -n ${NUM_IMG})
+echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut.py           -b all ${TAILCUT_PARAMS}       -o score_gamma_all_tailcut.json             $(find ~/astri_data/fits/gamma -type f -name "*.fits" | head -n ${NUM_IMG})
+echo "* GAMMA WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b all ${WT_MR_FILTER_PARAMS}  -o score_gamma_all_wavelets_mrfilter.json   $(find ~/astri_data/fits/gamma -type f -name "*.fits" | head -n ${NUM_IMG})
 for FILE in .tmp*.fits ; do rm $FILE ; done
 
 # ALL GAMMAS ######
 
-#echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b all ${TAILCUT_PARAMS}       -o score_gamma_mpdspd_tailcut_jd.json          ~/astri_data/fits/gamma
-#echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut.py           -b all ${TAILCUT_PARAMS}       -o score_gamma_mpdspd_tailcut.json             ~/astri_data/fits/gamma
-#echo "* GAMMA WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b all ${WT_MR_FILTER_PARAMS}  -o score_gamma_mpdspd_wavelets_mrfilter.json   ~/astri_data/fits/gamma
+#echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut_jd.py        -b all ${TAILCUT_PARAMS}       -o score_gamma_all_tailcut_jd.json          ~/astri_data/fits/gamma
+#echo "* GAMMA TAILCUT"  & ./datapipe/denoising/tailcut.py           -b all ${TAILCUT_PARAMS}       -o score_gamma_all_tailcut.json             ~/astri_data/fits/gamma
+#echo "* GAMMA WAVELETS" & ./datapipe/denoising/wavelets_mrfilter.py -b all ${WT_MR_FILTER_PARAMS}  -o score_gamma_all_wavelets_mrfilter.json   ~/astri_data/fits/gamma
 #for FILE in .tmp*.fits ; do rm $FILE ; done
