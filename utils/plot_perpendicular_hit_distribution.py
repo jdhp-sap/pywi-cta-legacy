65d1fe9c0db3956e076d44ba14264bc378f58852
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-01-20 19:24:22 +0100

    Misc updates.

--- utils/plot_perpendicular_hit_distribution.py
+++ utils/plot_perpendicular_hit_distribution.py
@@ -49,9 +49,9 @@ from ctapipe.image.hillas import hillas_parameters_1 as hillas_parameters_1
 from ctapipe.image.hillas import hillas_parameters_2 as hillas_parameters_2
 
 
-COLOR_MAP = "gnuplot2" # "gray"
+COLOR_MAP = "gray_r" # "gnuplot2" # "gray"
 
-def plot_image(axis, image_array, title, plot_log_scale=True):
+def plot_image(axis, image_array, title, plot_log_scale=False):
 
     #im = axis.imshow(image_array,
     #                 origin='lower',
@@ -65,6 +65,30 @@ def plot_image(axis, image_array, title, plot_log_scale=True):
 
     # generate 2 2d grids for the x & y bounds
     y, x = np.mgrid[slice(0, image_array.shape[0], dy), slice(0, image_array.shape[1], dx)]  # TODO !!!
+
+    z_min, z_max = image_array.min(), image_array.max()
+
+    if plot_log_scale:
+        # See http://matplotlib.org/examples/pylab_examples/pcolor_log.html
+        #     http://stackoverflow.com/questions/2546475/how-can-i-draw-a-log-normalized-imshow-plot-with-a-colorbar-representing-the-raw
+        im = axis.pcolor(x, y, image_array, norm=LogNorm(vmin=0.01, vmax=image_array.max()), cmap=COLOR_MAP)  # TODO: "vmin=0.01" is an arbitrary choice...
+    else:
+        im = axis.pcolor(x, y, image_array, cmap=COLOR_MAP, vmin=z_min, vmax=z_max)
+
+    plt.colorbar(im, ax=axis) # draw the colorbar
+
+    axis.set_title(title)
+
+
+def plot_image_meter(axis, image_array, pixels_position, title, plot_log_scale=False):
+
+    # See http://matplotlib.org/examples/pylab_examples/pcolor_demo.html
+
+    print(pixels_position)
+    print(pixels_position.shape)
+
+    # generate 2 2d grids for the x & y bounds
+    x, y = pixels_position[0], pixels_position[1]
 
     z_min, z_max = image_array.min(), image_array.max()
 
@@ -127,7 +151,7 @@ def plot_ellipse_shower_on_image(axis, image_array):
     axis.axes.add_patch(ellipse)
 
 
-def plot_perpendicular_hit_distribution(axis, image_array, title):
+def plot_perpendicular_hit_distribution(histogram_axis, image_axis, image_array, pixels_position, title):
 
     image_array = copy.deepcopy(image_array)
 
@@ -135,17 +159,21 @@ def plot_perpendicular_hit_distribution(axis, image_array, title):
 
     size_m = 0.2  # Size of the "phase space" in meter
 
-     # TODO: clean these following hard coded values for Astri
-    num_pixels_x = 40
-    num_pixels_y = 40
+#     # TODO: clean these following hard coded values for Astri
+#    num_pixels_x = 40
+#    num_pixels_y = 40
+#
+#    x = np.linspace(-0.142555996776, 0.142555996776, num_pixels_x)
+#    y = np.linspace(-0.142555996776, 0.142555996776, num_pixels_y)
+#
+#    #x = np.arange(0, np.shape(ref_image_array)[0], 1)          # TODO: wrong values -10 10 21
+#    #y = np.arange(0, np.shape(ref_image_array)[1], 1)          # TODO: wrong values  (30, ...)
+#
+#    xx, yy = np.meshgrid(x, y)
+#    print("delta x:", xx - pixels_position[0])
+#    print("delta y:", yy - pixels_position[1])
 
-    x = np.linspace(-0.142555996776, 0.142555996776, num_pixels_x)
-    y = np.linspace(-0.142555996776, 0.142555996776, num_pixels_y)
-
-    #x = np.arange(0, np.shape(ref_image_array)[0], 1)          # TODO: wrong values -10 10 21
-    #y = np.arange(0, np.shape(ref_image_array)[1], 1)          # TODO: wrong values  (30, ...)
-
-    xx, yy = np.meshgrid(x, y)
+    xx, yy = pixels_position[0], pixels_position[1]
 
     # Based on Tino's evaluate_cleaning.py (l. 277)
     hillas = hillas_parameters_1(xx.flatten() * u.meter,
@@ -158,17 +186,28 @@ def plot_perpendicular_hit_distribution(axis, image_array, title):
     p1_x = hillas.cen_x
     p1_y = hillas.cen_y
 
+    image_axis.scatter(p1_x, p1_y)  # DEBUG plot
+
     # p2 = intersection between the ellipse and the shower track
     p2_x = p1_x + hillas.length * np.cos(hillas.psi + np.pi/2)
     p2_y = p1_y + hillas.length * np.sin(hillas.psi + np.pi/2)
 
-    # slope of the shower track
-    T = linalg.normalise(np.array([p1_x-p2_x, p1_y-p2_y]))
+    image_axis.scatter(p2_x, p2_y)               # DEBUG plot
+    image_axis.plot([p1_x, p2_x], [p1_y, p2_y])  # DEBUG plot
+    print(p1_x, p2_x, p1_y, p2_y)                # DEBUG plot
+
+    # Slope of the shower track
+    T = linalg.normalise(np.array([p1_x-p2_x, p1_y-p2_y]))  # why a dedicated function ? if it's what I understand, it can easily be done on the fly
+
+    print("[p1_x-p2_x, p1_y-p2_y]:", [p1_x-p2_x, p1_y-p2_y])
+    print("T:", T)
+    image_axis.plot([p1_x, p1_x*T[0]], [p1_y, p1_y*T[1]], "-g", linewidth=3)  # DEBUG plot
 
     x = xx.flatten()
     y = yy.flatten()
 
     # Manhattan distance of pixels to the center of the ellipse
+    # Translate (center) on P1 ?
     D = [p1_x-x, p1_y-y]
 
     # Pixels in the new base
@@ -176,18 +215,26 @@ def plot_perpendicular_hit_distribution(axis, image_array, title):
     dp = D[0]*T[1] - D[1]*T[0]
 
     # nparray.ravel(): Return a flattened array.
-    values, bins, patches = axis.hist(dp.ravel(),
-                                      histtype='step',
-                                      bins=np.linspace(-size_m, size_m, 31))          # -10 10 21
+    values, bins, patches = histogram_axis.hist(dp.ravel(),
+                                                histtype='step',
+                                                bins=np.linspace(-size_m, size_m, 31))          # -10 10 21
+
+    # TODO: scatter seulement  les points qui sont dans le linspace de bins defini juste au dessus, faire apparaitre chaque bin d'une couleure différente
     
+    #image_axis.scatter(dl, dp, s=10, alpha=0.5)               # DEBUG plot
+    image_axis.scatter(dl, dp, s=10, alpha=0.5)               # DEBUG plot
+    image_axis.plot(dl.reshape(40, 40).T, dp.reshape(40, 40).T)               # DEBUG plot
+    print(dl)
+    print(dp.shape)
+
     ###
 
-    axis.set_xlim([-size_m, size_m])
+    histogram_axis.set_xlim([-size_m, size_m])
 
-    axis.set_xlabel('Distance to the shower axis (m)', fontsize=14)
-    axis.set_ylabel('Hits', fontsize=14)
+    histogram_axis.set_xlabel('Distance to the shower axis (m)', fontsize=14)
+    histogram_axis.set_ylabel('Hits', fontsize=14)
 
-    axis.set_title(title)
+    histogram_axis.set_title(title)
 
 
 def main():
@@ -229,23 +276,24 @@ def main():
 
             fits_images_dict, fits_metadata_dict = images.load_benchmark_images(input_file_path)
 
-            input_img = fits_images_dict["input_image"]
             reference_img = fits_images_dict["reference_image"]
-
-            if input_img.ndim != 2:
-                raise Exception("Unexpected error: the input FITS file should contain a 2D array.")
+            pixels_position = fits_images_dict["pixels_position"]
+            
+            reference_img[reference_img > 0] = 1   # simplify the image
 
             if reference_img.ndim != 2:
                 raise Exception("Unexpected error: the input FITS file should contain a 2D array.")
 
             # ASSESS OR PRINT THE CLEANED IMAGE ###############################
 
-            fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 9))
+            fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(16, 9))
 
             plot_image(ax1, reference_img, "Reference image")
             plot_ellipse_shower_on_image(ax1, reference_img)
 
-            plot_perpendicular_hit_distribution(ax2, reference_img, "Perpendicular hit distribution")
+            plot_image_meter(ax2, reference_img, pixels_position, "Reference image")
+
+            plot_perpendicular_hit_distribution(ax3, ax2, reference_img, pixels_position, "Perpendicular hit distribution")
 
             # PLOT AND SAVE ###################################################
 
