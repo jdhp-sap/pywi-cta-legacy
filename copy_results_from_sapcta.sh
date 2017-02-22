9bf084d33e6d8409c3127feff2c3762aae6067b5
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-02-22 14:28:05 +0100

    Add a tool script.

---
+++ copy_results_from_sapcta.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# See: http://stackoverflow.com/questions/1228466/how-to-filter-files-when-using-scp-to-copy-dir-recursively
+rsync  -e ssh -rav --exclude='*/.git/' --exclude='*/utils/' --exclude='*/tests/' --exclude='*/docs/' --exclude='*/datapipe/' --include '*/' --include='*.json' --exclude='*' sapcta:/home/jdecock/xp/ ./xps/2017_02_22_sapcta/
