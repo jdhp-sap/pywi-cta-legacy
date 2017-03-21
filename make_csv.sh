4a89a00056f3b4039f9f4b977bfc1e3471035b46
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-03-21 13:32:53 +0100

    Fix a bug.

--- make_csv.sh
+++ make_csv.sh
@@ -3,13 +3,13 @@
 export PYTHONPATH=.:$PYTHONPATH
 source activate cta
 
-INPUT_DIR=./xps/2017_MM_DD_sapcta/
+INPUT_DIR=./xps/2017_MM_DD_sapcta
 OUTPUT_FILE=./xps/best.csv
 
 ./utils/benchmark_json_to_flat_v2.py -o "${OUTPUT_FILE}" \
-    ${INPUT_FILE}/score_*_ref.json \
-    ${INPUT_FILE}/score_*_input.json \
-    ${INPUT_FILE}/score_*_tc.json \
-    ${INPUT_FILE}/score_*_WT-K-k-C1-m3-n4-s3.json \
-    ${INPUT_FILE}/score_*_WT-K-k-C1-m3-n4-s2-2-3-3.json
+    ${INPUT_DIR}/score_*_ref.json \
+    ${INPUT_DIR}/score_*_input.json \
+    ${INPUT_DIR}/score_*_tc.json \
+    ${INPUT_DIR}/score_*_WT-K-k-C1-m3-n4-s3.json \
+    ${INPUT_DIR}/score_*_WT-K-k-C1-m3-n4-s2-2-3-3.json
 
