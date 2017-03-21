e63bc1b95d83bdd48f3aa0ba500edaedb5bdf99a
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-03-21 14:03:40 +0100

    Misc updates.

--- copy_results_from_sapcta.sh
+++ copy_results_from_sapcta.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-OUTPUT_DIR=./xps/2017_MM_DD_sapcta/
+OUTPUT_DIR=./xps/2017_MM_DD_sapcta
 
-mkdir "${OUTPUT_DIR}"
+mkdir ${OUTPUT_DIR}
 
 # See: http://stackoverflow.com/questions/1228466/how-to-filter-files-when-using-scp-to-copy-dir-recursively
-rsync  -e ssh -rav --exclude='*/.git/' --exclude='*/utils/' --exclude='*/tests/' --exclude='*/docs/' --exclude='*/datapipe/' --include '*/' --include='*.json' --exclude='*'     sapcta:/home/jdecock/xp/ "${OUTPUT_DIR}"
-rsync  -e ssh -rav --exclude='*/.git/' --exclude='*/utils/' --exclude='*/tests/' --exclude='*/docs/' --exclude='*/datapipe/' --include '*/' --include='*.json.log' --exclude='*' sapcta:/home/jdecock/xp/ "${OUTPUT_DIR}"
+rsync  -e ssh -rav --exclude='*/.git/' --exclude='*/utils/' --exclude='*/tests/' --exclude='*/docs/' --exclude='*/datapipe/' --include '*/' --include='*.json' --exclude='*'     sapcta:/home/jdecock/xp/ ${OUTPUT_DIR}/
+rsync  -e ssh -rav --exclude='*/.git/' --exclude='*/utils/' --exclude='*/tests/' --exclude='*/docs/' --exclude='*/datapipe/' --include '*/' --include='*.json.log' --exclude='*' sapcta:/home/jdecock/xp/ ${OUTPUT_DIR}/
 
 # Remove empty directories
 mv    ${OUTPUT_DIR}/run*/*.json* ${OUTPUT_DIR}/
