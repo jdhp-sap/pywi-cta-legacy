ad8c98cd98340054236e4a1450d1eebc8f2f95a7
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-04-21 20:08:46 +0200

    Add a function to convert ASTRI simtel data to square grid fits files without cropping borders.

--- datapipe/io/geometry_converter.py
+++ datapipe/io/geometry_converter.py
@@ -28,7 +28,85 @@ __all__ = ['astry_to_2d_array']
 
 import numpy as np
 
-def astry_to_2d_array(input_img):
+def astry_to_2d_array(input_img, crop=True):
+    if crop:
+        return astry_to_2d_array_crop(input_img)
+    else:
+        return astry_to_2d_array_no_crop(input_img)
+
+
+def astry_to_2d_array_no_crop(input_img):
+    """
+    Crop images comming form "ASTRI" telescopes in order to get regular 2D "rectangular"
+    images directly usable with most image processing tools.
+
+    Parameters
+    ----------
+    input_img : numpy.array
+        The image to crop
+
+    Returns
+    -------
+    A numpy.array containing the cropped image.
+    """
+
+    # Check the image
+    if len(input_img) != (37*64):
+        raise ValueError("The input image is not a valide ASTRI telescope image.")
+
+    # Make the transformation map
+    img_map = np.zeros([8*7, 8*7], dtype=int)
+
+    img_map[0*8:1*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] + 34 * 64
+    img_map[0*8:1*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] + 35 * 64
+    img_map[0*8:1*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] + 36 * 64
+
+    img_map[1*8:2*8, 1*8:2*8] = np.arange(64).reshape([8,8])[::-1,:] + 29 * 64
+    img_map[1*8:2*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] + 30 * 64
+    img_map[1*8:2*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] + 31 * 64
+    img_map[1*8:2*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] + 32 * 64
+    img_map[1*8:2*8, 5*8:6*8] = np.arange(64).reshape([8,8])[::-1,:] + 33 * 64
+
+    img_map[2*8:3*8, 0*8:1*8] = np.arange(64).reshape([8,8])[::-1,:] + 22 * 64
+    img_map[2*8:3*8, 1*8:2*8] = np.arange(64).reshape([8,8])[::-1,:] + 23 * 64
+    img_map[2*8:3*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] + 24 * 64
+    img_map[2*8:3*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] + 25 * 64
+    img_map[2*8:3*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] + 26 * 64
+    img_map[2*8:3*8, 5*8:6*8] = np.arange(64).reshape([8,8])[::-1,:] + 27 * 64
+    img_map[2*8:3*8, 6*8:7*8] = np.arange(64).reshape([8,8])[::-1,:] + 28 * 64
+
+    img_map[3*8:4*8, 0*8:1*8] = np.arange(64).reshape([8,8])[::-1,:] + 15 * 64
+    img_map[3*8:4*8, 1*8:2*8] = np.arange(64).reshape([8,8])[::-1,:] + 16 * 64
+    img_map[3*8:4*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] + 17 * 64
+    img_map[3*8:4*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] + 18 * 64
+    img_map[3*8:4*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] + 19 * 64
+    img_map[3*8:4*8, 5*8:6*8] = np.arange(64).reshape([8,8])[::-1,:] + 20 * 64
+    img_map[3*8:4*8, 6*8:7*8] = np.arange(64).reshape([8,8])[::-1,:] + 21 * 64
+
+    img_map[4*8:5*8, 0*8:1*8] = np.arange(64).reshape([8,8])[::-1,:] +  8 * 64
+    img_map[4*8:5*8, 1*8:2*8] = np.arange(64).reshape([8,8])[::-1,:] +  9 * 64
+    img_map[4*8:5*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] + 10 * 64
+    img_map[4*8:5*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] + 11 * 64
+    img_map[4*8:5*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] + 12 * 64
+    img_map[4*8:5*8, 5*8:6*8] = np.arange(64).reshape([8,8])[::-1,:] + 13 * 64
+    img_map[4*8:5*8, 6*8:7*8] = np.arange(64).reshape([8,8])[::-1,:] + 14 * 64
+
+    img_map[5*8:6*8, 1*8:2*8] = np.arange(64).reshape([8,8])[::-1,:] +  3 * 64
+    img_map[5*8:6*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] +  4 * 64
+    img_map[5*8:6*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] +  5 * 64
+    img_map[5*8:6*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] +  6 * 64
+    img_map[5*8:6*8, 5*8:6*8] = np.arange(64).reshape([8,8])[::-1,:] +  7 * 64
+
+    img_map[6*8:7*8, 2*8:3*8] = np.arange(64).reshape([8,8])[::-1,:] +  0 * 64
+    img_map[6*8:7*8, 3*8:4*8] = np.arange(64).reshape([8,8])[::-1,:] +  1 * 64
+    img_map[6*8:7*8, 4*8:5*8] = np.arange(64).reshape([8,8])[::-1,:] +  2 * 64
+
+    # Make the output image
+    cropped_img = input_img[[img_map.ravel()]].reshape([8*7, 8*7])
+
+    return cropped_img
+
+def astry_to_2d_array_crop(input_img):
     """
     Crop images comming form "ASTRI" telescopes in order to get regular 2D "rectangular"
     images directly usable with most image processing tools.
@@ -86,7 +164,7 @@ def astry_to_2d_array(input_img):
     return cropped_img
 
 
-def astry_to_3d_array(input_img):
+def astry_to_3d_array(input_img, crop=True):
     """
     Crop images comming form "ASTRI" telescopes in order to get regular 2D "rectangular"
     images directly usable with most image processing tools.
