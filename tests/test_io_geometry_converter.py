3896067f335f9fac9f07745a4c25ba4efe35e6ca
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-05-10 14:05:59 +0200

    Add a unit test.

--- tests/test_io_geometry_converter.py
+++ tests/test_io_geometry_converter.py
@@ -28,6 +28,8 @@ import ctapipe
 from ctapipe.io.hessio import hessio_event_source
 import pyhessio
 
+import datapipe
+import datapipe.io.images
 from datapipe.io import geometry_converter
 
 # Old version
@@ -40,6 +42,9 @@ import numpy as np
 
 import unittest
 
+SIMTEL_FILE_PATH = "/Users/jdecock/data/astri_mini_array/proton/run10001.simtel.gz"
+FITS_FILE_PATH = "/Users/jdecock/data/astri_mini_array/fits/gamma/run1001.simtel.gz_TEL001_EV155601.fits"
+
 class TestGeometryConverter(unittest.TestCase):
     """
     Contains unit tests for the "io.geometry_converter" module.
@@ -49,10 +54,9 @@ class TestGeometryConverter(unittest.TestCase):
 
     def test_geom_json_dict_ex1(self):
 
-        simtel_file_path = "/Users/jdecock/data/astri_mini_array/proton/run10001.simtel.gz"
         tel_id = 1
 
-        source = hessio_event_source(simtel_file_path, allowed_tels=[tel_id])
+        source = hessio_event_source(SIMTEL_FILE_PATH, allowed_tels=[tel_id])
 
         for ev in source:
             event = ev
@@ -85,11 +89,10 @@ class TestGeometryConverter(unittest.TestCase):
 
     def test_geom_json_file_ex1(self):
 
-        simtel_file_path = "/Users/jdecock/data/astri_mini_array/proton/run10001.simtel.gz"
         tel_id = 1
         output_json_file = "/tmp/astri.geom.json"
 
-        source = hessio_event_source(simtel_file_path, allowed_tels=[tel_id])
+        source = hessio_event_source(SIMTEL_FILE_PATH, allowed_tels=[tel_id])
 
         for ev in source:
             event = ev
@@ -116,6 +119,53 @@ class TestGeometryConverter(unittest.TestCase):
         np.testing.assert_array_equal(geom2.pix_area.value, geom.pix_area.value)
         self.assertEqual(geom2.neighbors, geom.neighbors)
         self.assertEqual(geom2.pix_type,  geom.pix_type)
+
+
+    # Test the "astri_to_2d_array_no_crop" and "" functions ################################
+
+    def test_1d_2d_1d_ex1(self):
+
+        tel_id = 1
+        source = hessio_event_source(SIMTEL_FILE_PATH, allowed_tels=[tel_id])
+
+        for ev in source:
+            event = ev
+
+        # Get the 1D image
+
+        img_1d = event.mc.tel[tel_id].photo_electron_image
+
+        # Convert to 2D image
+
+        img_2d = geometry_converter.astri_to_2d_array_no_crop(img_1d)
+
+        # Convert to 1D image
+
+        img_1d_v2 = geometry_converter.array_2d_to_astri_no_crop(img_2d)
+
+        # Check whether the input image has changed
+
+        np.testing.assert_array_equal(img_1d, img_1d_v2)
+
+
+    def test_2d_1d_2d_ex1(self):
+
+        # Get the 2D image
+
+        imgs, metadata_dict = datapipe.io.images.load_benchmark_images(FITS_FILE_PATH)
+        img_2d = imgs['input_image']
+
+        # Convert to 1D image
+
+        img_1d = geometry_converter.array_2d_to_astri_no_crop(img_2d)
+
+        # Convert to 2D image
+
+        img_2d_v2 = geometry_converter.astri_to_2d_array_no_crop(img_1d)
+
+        # Check whether the input image has changed
+
+        np.testing.assert_array_equal(img_2d, img_2d_v2)
     
 
 if __name__ == '__main__':
