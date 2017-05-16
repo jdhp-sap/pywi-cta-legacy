7bec2252c44e25012a6d3aa68d24e87125599278
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-05-16 11:57:17 +0200

    Move geom files.

--- datapipe/io/geom/__init__.py
+++ datapipe/io/geom/__init__.py
@@ -20,8 +20,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-__all__ = ['geometry_converter',
-           'geom',
-           'images',
-           'montecarlo_calibration',
-           'simtel']
+__all__ = []
