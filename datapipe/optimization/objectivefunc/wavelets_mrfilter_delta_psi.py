b41e415c84a55f434ae7d2ba3ee2d241a553245b
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-08-04 18:48:09 +0200

    Fill a module.

--- datapipe/optimization/objectivefunc/wavelets_mrfilter_delta_psi.py
+++ datapipe/optimization/objectivefunc/wavelets_mrfilter_delta_psi.py
@@ -22,37 +22,138 @@
 
 __all__ = ['ObjectiveFunction']
 
-from datapipe.denoising import WaveletTransform
+import numpy as np
+
+from datapipe.denoising.wavelets_mrfilter import WaveletTransform
+from datapipe.benchmark import assess
 
 # OPTIMIZER ##################################################################
 
 class ObjectiveFunction:
 
-    def __init__(self):
+    def __init__(self, input_files):
         self.call_number = 0
 
-    def __call__(self, cleaning_function_params, input_file_or_dir_path_list):
+        # Init the wavelet class
+        self.cleaning_algorithm = WaveletTransform()
+
+        # Make the image list
+        self.input_files = input_files
+
+        # PRE PROCESSING FILTERING ############################################
+
+        # TODO...
+
+
+    def __call__(self, algo_params_var):
         self.call_number += 1
 
         benchmark_method = "all"          # TODO
-        label = "WT" + self.call_number   # TODO
 
-        cleaning_algorithm.label = label
+        label = "WT_{}".format(self.call_number)
+        self.cleaning_algorithm.label = label
 
         output_file_path = "score_wavelets_optim_{}.json".format(self.call_number)
 
-        cleaning_algorithm = WaveletTransform()
+        algo_params = {
+                    "coef_detection_method": 1,
+                    "correction_offset": False,
+                    "detect_only_positive_structure": False,
+                    "epsilon": None,
+                    "first_detection_scale": None,
+                    "input_image_scale": "linear",
+                    #"k_sigma_noise_threshold": "2,2,3,3",
+                    "kill_isolated_pixels": True,
+                    "mask_file_path": None,
+                    #"mrfilter_directory": "/Volumes/ramdisk",
+                    "nan_noise_lambda": 0,
+                    "nan_noise_mu": 0,
+                    "nan_noise_sigma": 0,
+                    "noise_model": 3,
+                    "number_of_iterations": None,
+                    "number_of_scales": 4,
+                    "offset_after_calibration": None,
+                    "precision": None,
+                    "support_file_name": None,
+                    "suppress_isolated_pixels": True,
+                    "suppress_last_scale": True,
+                    "suppress_positivity_constraint": False,
+                    "tmp_files_directory": "/Volumes/ramdisk",
+                    "type_of_filtering": None,
+                    "type_of_filters": None,
+                    "type_of_multiresolution_transform": None,
+                    "type_of_non_orthog_filters": None,
+                    "verbose": False
+                }
 
-        output_dict = cleaning_algorithm.run(cleaning_function_params,
-                                             input_file_or_dir_path_list,
-                                             benchmark_method,
-                                             output_file_path,
-                                             plot=False,
-                                             saveplot=False)
+        algo_params.update(algo_params_var)
 
-        # Convert output_dict to error (e.g. compute the mean delta_psi per range of energy)
-        print(output_dict)
-        error = None      # TODO
+        # TODO: randomly make a subset fo self.input_files
+        input_files = self.input_files
 
-        return error
+        output_dict = self.cleaning_algorithm.run(algo_params,
+                                                  input_file_or_dir_path_list=input_files,
+                                                  benchmark_method=benchmark_method,
+                                                  output_file_path=output_file_path)
+
+        #print(output_dict)
+
+        score_list = []
+
+        # Read and compute results from output_dict
+        for image_dict in output_dict["io"]:
+
+            # POST PROCESSING FILTERING #######################################
+
+            # >>>TODO<<<: Filter images: decide wether the image should be used or not ? (contained vs not contained)
+            # TODO: filter these images *before* cleaning them to avoid waste of computation...
+
+            # >>>TODO<<<: Filter images by energy range: decide wether the image should be used or not ?
+            # TODO: filter these images *before* cleaning them to avoid waste of computation...
+
+            ###################################################################
+
+            # !!!>>>TODO<<<!!!: Asses the cleaned image
+#            wt_hillas_score  = assess.metric_hillas_delta2(input_img, output_image, reference_image, pixels_position, params=None)
+#            ref_hillas_score = assess.metric_hillas_delta2(ref_img,   output_image, reference_image, pixels_position, params=None)
+#
+#            wt_psi  = hillas_params_2_cleaned_img.psi.to(u.rad).value
+#            ref_psi = hillas_params_2_cleaned_img.psi.to(u.rad).value
+#
+#            # Compute and normalize the delta psi
+#            delta_psi = np.fmod(((ref_psi - wt_psi) * 180. / np.pi), 90.)
+#            delta_psi = abs(delta_psi)
+
+            #score_index = image_dict["score_name"].find("hillas2_delta_psi")
+            if "img_cleaned_hillas_2_psi" in image_dict:
+                delta_psi = image_dict["img_cleaned_hillas_2_psi"]  # image_dict["score"][score_index]   # TODO: WHY ARE THERE MULTIPLE RESULTS "DELTAPSI": image_dict["img_cleaned_hillas_2_psi"], image_dict["score"][...], ... ???
+                score_list.append(delta_psi)
+
+        # Compute the mean
+        score = np.array([score_list]).mean()
+
+        # TODO: save results in a JSON file (?)
+
+        return score
+
+
+if __name__ == "__main__":
+    # Test...
+
+    #func = ObjectiveFunction(input_files=["/Users/jdecock/astri_data/fits/gamma/"])
+    func = ObjectiveFunction(input_files=["./testset/gamma/astri/tel1/"])
+
+    algo_params_var = {
+                "k_sigma_noise_threshold": "2"
+            }
+
+    score = func(algo_params_var)
+
+    #algo_params_var = {
+    #            "k_sigma_noise_threshold": "3"
+    #        }
+
+    #score = func(algo_params_var)
+
+    print(score)
 
