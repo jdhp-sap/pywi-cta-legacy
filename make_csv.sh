44bd2a4dd86fc2631d28dad2691e541dc6bd44af
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-08-11 11:42:20 +0200

    Improve a script.

--- make_csv.sh
+++ make_csv.sh
@@ -3,15 +3,12 @@
 export PYTHONPATH=.:$PYTHONPATH
 source activate cta
 
-NAME=2017_MM_DD
-
-INPUT_DIR=./xps/${NAME}_sapcta
-OUTPUT_FILE=./xps/${NAME}.csv
+INPUT_DIR="$1"
+OUTPUT_FILE="$2"
 
 ./utils/benchmark_json_to_flat_v2.py -o "${OUTPUT_FILE}" \
     ${INPUT_DIR}/score_*_ref.json \
     ${INPUT_DIR}/score_*_input.json \
     ${INPUT_DIR}/score_*_tc.json \
-    ${INPUT_DIR}/score_*_WT-K-k-C1-m3-n4-s3.json \
-    ${INPUT_DIR}/score_*_WT-K-k-C1-m3-n4-s2-2-3-3.json
+    ${INPUT_DIR}/score_*_WT*.json
 
