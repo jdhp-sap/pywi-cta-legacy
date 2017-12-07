7b38828f8c595898fa3e6f604b1016ddafd33d6e
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-12-07 18:25:19 +0100

    Misc update.

--- utils/make_csv.sh
+++ utils/make_csv.sh
@@ -9,6 +9,6 @@ OUTPUT_FILE="$2"
 ./utils/benchmark_json_to_flat_v2.py -o "${OUTPUT_FILE}" \
     ${INPUT_DIR}/score_*_ref.json \
     ${INPUT_DIR}/score_*_input.json \
-    ${INPUT_DIR}/score_*_tc.json \
+    ${INPUT_DIR}/score_*_Tailcut*.json \
     ${INPUT_DIR}/score_*_WT*.json
 
