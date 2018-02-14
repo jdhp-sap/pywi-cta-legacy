c86de974f262e27e325984cd1f34ed8eb465c7a0
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-02-14 19:10:39 +0100

    Include mr_transform.

--- xp.sh
+++ xp.sh
@@ -46,9 +46,9 @@ macos)
     export PYTHONPATH=.:$PYTHONPATH ;
     if [ -d /Volumes/ramdisk ]
     then
-        MRFILTER_TMP_DIR="/Volumes/ramdisk"
+        MR_TMP_DIR="/Volumes/ramdisk"
     else
-        MRFILTER_TMP_DIR="."
+        MR_TMP_DIR="."
         echo "*** WARNING: CANNOT USE RAMDISK FOR TEMPORARY FILES ; USE ./ INSTEAD... ***"
     fi
     ;;
@@ -56,9 +56,9 @@ linux)
     export PYTHONPATH=.:~/git/pub/ext/ctapipe-extra:$PYTHONPATH ;
     if [ -d /dev/shm/.jd ]
     then
-        MRFILTER_TMP_DIR="/dev/shm/.jd"
+        MR_TMP_DIR="/dev/shm/.jd"
     else
-        MRFILTER_TMP_DIR="."
+        MR_TMP_DIR="."
         echo "*** WARNING: CANNOT USE RAMDISK FOR TEMPORARY FILES ; USE ./ INSTEAD... ***"
     fi
     ;;
@@ -68,17 +68,17 @@ linux)
     ;;
 esac
 
-echo "MRFILTER_TMP_DIR: ${MRFILTER_TMP_DIR}"
+echo "MR_TMP_DIR: ${MR_TMP_DIR}"
 
 # DENOISING PARAMETERS AND INPUT FILES ########################################
 
 # TODO
-#GAMMA_FITS_DIR=${MRFILTER_TMP_DIR}/astri_data/fits/gamma
-#PROTON_FITS_DIR=${MRFILTER_TMP_DIR}/astri_data/fits/proton
+#GAMMA_FITS_DIR=${MR_TMP_DIR}/astri_data/fits/gamma
+#PROTON_FITS_DIR=${MR_TMP_DIR}/astri_data/fits/proton
 
 # TODO
-#GAMMA_FITS_DIR=${MRFILTER_TMP_DIR}/astri_data/fits_flashcam/gamma
-#PROTON_FITS_DIR=${MRFILTER_TMP_DIR}/astri_data/fits_flashcam/proton
+#GAMMA_FITS_DIR=${MR_TMP_DIR}/astri_data/fits_flashcam/gamma
+#PROTON_FITS_DIR=${MR_TMP_DIR}/astri_data/fits_flashcam/proton
 
 case ${INST} in
 astri_mini_inaf)
@@ -102,16 +102,16 @@ astri_mini_inaf)
     TC_LABEL="Tailcut-CTA2-${TC_HTH}-${TC_LTH}" ;
 
     ## 2016
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s3       --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s3"
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s3       --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s3"
 
     ## 2017/02 (presented in LaPalma CTA meeting 2017-11-05)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s2,2,3,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s2-2-3-3" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s2,2,3,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s2-2-3-3" ;
 
     # 2017/09/07 (BF res0.5)
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s1,1,2,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s1-1-2-1" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s1,1,2,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s1-1-2-1" ;
     ;;
 astri_mini_konrad)
     CAM_ID="ASTRICam"
@@ -136,12 +136,12 @@ astri_mini_konrad)
     TC_LABEL="Tailcut-CTA2-${TC_HTH}-${TC_LTH}" ;
 
     ## 2017/02 (presented in LaPalma CTA meeting 2017-11-05)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s2,2,3,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s2-2-3-3" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s2,2,3,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s2-2-3-3" ;
 
     # 2017/09/07 (BF res0.5)
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s3,1,3.5,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s3-1-3.5-1" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s3,1,3.5,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s3-1-3.5-1" ;
     ;;
 gct_mini_konrad)
     CAM_ID="CHEC"
@@ -165,8 +165,8 @@ gct_mini_konrad)
     TC_LTH="1" ; # CTA Abelardo 2nd pass
     TC_LABEL="Tailcut-CTA2-${TC_HTH}-${TC_LTH}" ;
 
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s2,2,3,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s2-2-3-3" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s2,2,3,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s2-2-3-3" ;
     ;;
 digicam_mini_konrad)
     CAM_ID="DigiCam"
@@ -193,12 +193,12 @@ digicam_mini_konrad)
     TC_LABEL="Tailcut-BF1-${TC_HTH}-${TC_LTH}" ;
 
     ## 2017/09/11 (BF res1) (presented in LaPalma CTA meeting 2017-11-05)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s3,3,4,4 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s3-3-4-4" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s3,3,4,4 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s3-3-4-4" ;
 
     # 2017/10/24 (SAES)
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s6.274,2.629,7.755,0.076 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s6.274-2.629-7.755-0.076" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s6.274,2.629,7.755,0.076 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s6.274-2.629-7.755-0.076" ;
     ;;
 flashcam_mini_inaf)
     CAM_ID="FlashCam"
@@ -221,12 +221,12 @@ flashcam_mini_inaf)
     TC_LABEL="Tailcut-CTA2-${TC_HTH}-${TC_LTH}" ;
 
     ## 2017/07 (presented in LaPalma CTA meeting 2017-11-05)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s4,4,5,4 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s4-4-5-4" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s4,4,5,4 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s4-4-5-4" ;
 
     # 2017/09/07 (BF res0.5)
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s4.5,4.5,4.5,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s4.5-4.5-4.5-1" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s4.5,4.5,4.5,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s4.5-4.5-4.5-1" ;
     ;;
 nectarcam_grid_prod3b_north)
     CAM_ID="NectarCam"
@@ -253,16 +253,16 @@ nectarcam_grid_prod3b_north)
     TC_LABEL="Tailcut-BF1-${TC_HTH}-${TC_LTH}" ;
 
     ## 2017/08
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s2,4.5,3.5,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s2-4.5-3.5-3" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s2,4.5,3.5,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s2-4.5-3.5-3" ;
 
     ## 2017/09/07 (BF res0.5) (presented in LaPalma CTA meeting 2017-11-05)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s3,2.5,4,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s3-2.5-4-1" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s3,2.5,4,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s3-2.5-4-1" ;
 
     # 2017/10/24 (SAES)
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s13.013,2.549,6.559,1.412 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s13.013-2.549-6.559-1.412" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s13.013,2.549,6.559,1.412 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s13.013-2.549-6.559-1.412" ;
     ;;
 lstcam_grid_prod3b_north)
     CAM_ID="LSTCam"
@@ -273,6 +273,8 @@ lstcam_grid_prod3b_north)
     #PROTON_FITS_DIR=~/data/grid_prod3b_north/simtel/proton ;
 
     WT_NAN_NOISE_CDF_FILE=./datapipe/denoising/cdf/lstcam_grid_prod3b_north_cdf_gamma.json ;
+
+    # TAILCUT #################################################################
 
     #TC_HTH="10" ; # HESS
     #TC_LTH="5" ;  # HESS
@@ -290,17 +292,25 @@ lstcam_grid_prod3b_north)
     TC_LTH="1" ;   # 2018/01/09 Brutforce Delta psi optim (res 1.0, 1000 img, mean, 30-2000 PE, missing img penalty: 90)
     TC_LABEL="Tailcut-BF1-${TC_HTH}-${TC_LTH}" ;
 
+    # MR_FILTER ###############################################################
+
     ## 2017/08 (presented in LaPalma CTA meeting 2017-11-05)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s2,4.5,3.5,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s2-4.5-3.5-3" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s2,4.5,3.5,3 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s2-4.5-3.5-3" ;
 
     ## 2017/09/07 (BF res0.5)
-    #WT_PARAMS="-K -k -C1 -m3 -n4 -s2,2.5,4,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    #WT_LABEL="WT-K-k-C1-m3-n4-s2-2.5-4-1" ;
+    #WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s2,2.5,4,1 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    #WT_MRF_LABEL="WT-K-k-C1-m3-n4-s2-2.5-4-1" ;
 
     # 2017/10/24 (SAES)
-    WT_PARAMS="-K -k -C1 -m3 -n4 -s23.343,2.490,-2.856,-0.719 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MRFILTER_TMP_DIR}" ;
-    WT_LABEL="WT-K-k-C1-m3-n4-s23.343-2.490--2.856--0.719" ;
+    WT_MRF_PARAMS="-K -k -C1 -m3 -n4 -s23.343,2.490,-2.856,-0.719 --kill-isolated-pixels --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRF_LABEL="WT-K-k-C1-m3-n4-s23.343-2.490--2.856--0.719" ;
+
+    # MR_TRANSFORM ############################################################
+
+    # 2017/02/14
+    WT_MRT_PARAMS="-f hard_filtering -t 23.7,3.25,1. -L mask -p --noise-cdf-file=${WT_NAN_NOISE_CDF_FILE} --tmp-dir=${MR_TMP_DIR}" ;
+    WT_MRT_LABEL="WT_MRT_-f_hard_filtering_-t_23.7_3.25_1._-L_mask_-p" ;
     ;;
 *)
     echo "Unknown option" ;
@@ -312,8 +322,10 @@ TC_PARAMS="-T${TC_HTH} -t${TC_LTH} --kill-isolated-pixels" ;
 
 echo "TC_PARAMS: ${TC_PARAMS}"
 echo "TC_LABEL:  ${TC_LABEL}"
-echo "WT_PARAMS: ${WT_PARAMS}"
-echo "WT_LABEL:  ${WT_LABEL}"
+echo "WT_MRF_PARAMS: ${WT_MRF_PARAMS}"
+echo "WT_MRF_LABEL:  ${WT_MRF_LABEL}"
+echo "WT_MRT_PARAMS: ${WT_MRT_PARAMS}"
+echo "WT_MRT_LABEL:  ${WT_MRT_LABEL}"
 
 echo "GAMMA_FITS_DIR:  ${GAMMA_FITS_DIR}"
 echo "PROTON_FITS_DIR: ${PROTON_FITS_DIR}"
@@ -340,18 +352,22 @@ sleep 5
 # ALL GAMMAS ######
 ###################
 
-echo "* NULL (REF.)"  & ./datapipe/denoising/null_ref.py          -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Ref"                      -o score_gamma_ref.json          ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_all_null_ref.json.log ;
-echo "* NULL (INPUT)" & ./datapipe/denoising/null.py              -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Input"                    -o score_gamma_input.json        ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_all_null_input.json.log ;
-echo "* GAMMA TC"     & ./datapipe/denoising/tailcut.py           -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${TC_LABEL}" ${TC_PARAMS} -o score_gamma_${TC_LABEL}.json  ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_${TC_LABEL}.json.log ;
-echo "* GAMMA WT"     & ./datapipe/denoising/wavelets_mrfilter.py -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${WT_LABEL}" ${WT_PARAMS} -o score_gamma_${WT_LABEL}.json  ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_${WT_LABEL}.json.log ;
-for FILE in ${MRFILTER_TMP_DIR}/.tmp*.fits ; do rm $FILE ; done
+echo "* NULL (REF.)"  & ./datapipe/denoising/null_ref.py             -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Ref"                      -o score_gamma_ref.json          ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_all_null_ref.json.log ;
+echo "* NULL (INPUT)" & ./datapipe/denoising/null.py                 -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Input"                    -o score_gamma_input.json        ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_all_null_input.json.log ;
+echo "* GAMMA TC"     & ./datapipe/denoising/tailcut.py              -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${TC_LABEL}" ${TC_PARAMS} -o score_gamma_${TC_LABEL}.json  ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_${TC_LABEL}.json.log ;
+#echo "* GAMMA WT MRF" & ./datapipe/denoising/wavelets_mrfilter.py    -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${WT_MRF_LABEL}" ${WT_MRF_PARAMS} -o score_gamma_${WT_MRF_LABEL}.json  ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_${WT_MRF_LABEL}.json.log ;
+#for FILE in ${MR_TMP_DIR}/.tmp*.fits ; do rm $FILE ; done
+echo "* GAMMA WT MRT" & ./datapipe/denoising/wavelets_mrtransform.py -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${WT_MRT_LABEL}" ${WT_MRT_PARAMS} -o score_gamma_${WT_MRT_LABEL}.json  ${GAMMA_FITS_DIR} 2>&1 | tee score_gamma_${WT_MRT_LABEL}.json.log ;
+for FILE in ${MR_TMP_DIR}/.tmp*.fits ; do rm $FILE ; done
 
 #####################
 ## ALL PROTONS ######
 #####################
 
-echo "* NULL (REF.)"  & ./datapipe/denoising/null_ref.py          -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Ref"                      -o score_proton_ref.json         ${PROTON_FITS_DIR} 2>&1 | tee score_proton_all_null_ref.json.log ;
-echo "* NULL (INPUT)" & ./datapipe/denoising/null.py              -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Input"                    -o score_proton_input.json       ${PROTON_FITS_DIR} 2>&1 | tee score_proton_all_null_input.json.log ;
-echo "* PROTON TC"    & ./datapipe/denoising/tailcut.py           -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${TC_LABEL}" ${TC_PARAMS} -o score_proton_${TC_LABEL}.json ${PROTON_FITS_DIR} 2>&1 | tee score_proton_${TC_LABEL}.json.log ;
-echo "* PROTON WT"    & ./datapipe/denoising/wavelets_mrfilter.py -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${WT_LABEL}" ${WT_PARAMS} -o score_proton_${WT_LABEL}.json ${PROTON_FITS_DIR} 2>&1 | tee score_proton_${WT_LABEL}.json.log ;
-for FILE in ${MRFILTER_TMP_DIR}/.tmp*.fits ; do rm $FILE ; done
+echo "* NULL (REF.)"   & ./datapipe/denoising/null_ref.py             -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Ref"                      -o score_proton_ref.json         ${PROTON_FITS_DIR} 2>&1 | tee score_proton_all_null_ref.json.log ;
+echo "* NULL (INPUT)"  & ./datapipe/denoising/null.py                 -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="Input"                    -o score_proton_input.json       ${PROTON_FITS_DIR} 2>&1 | tee score_proton_all_null_input.json.log ;
+echo "* PROTON TC"     & ./datapipe/denoising/tailcut.py              -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${TC_LABEL}" ${TC_PARAMS} -o score_proton_${TC_LABEL}.json ${PROTON_FITS_DIR} 2>&1 | tee score_proton_${TC_LABEL}.json.log ;
+#echo "* PROTON WT MRF" & ./datapipe/denoising/wavelets_mrfilter.py    -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${WT_MRF_LABEL}" ${WT_MRF_PARAMS} -o score_proton_${WT_MRF_LABEL}.json ${PROTON_FITS_DIR} 2>&1 | tee score_proton_${WT_MRF_LABEL}.json.log ;
+#for FILE in ${MR_TMP_DIR}/.tmp*.fits ; do rm $FILE ; done
+echo "* PROTON WT MRT" & ./datapipe/denoising/wavelets_mrtransform.py -b all --max-images ${NUM_IMG} --camid ${CAM_ID} --label="${WT_MRT_LABEL}" ${WT_MRT_PARAMS} -o score_proton_${WT_MRT_LABEL}.json ${PROTON_FITS_DIR} 2>&1 | tee score_proton_${WT_MRT_LABEL}.json.log ;
+for FILE in ${MR_TMP_DIR}/.tmp*.fits ; do rm $FILE ; done
